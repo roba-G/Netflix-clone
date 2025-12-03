ff39c936a070801430e704ee81da86dc0862a191
roba996 <124337124+roba996@users.noreply.github.com>
2025-12-03 15:07:42 +0300

    banner added

--- netflix-clone/src/pages/Home.jsx
+++ netflix-clone/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header/Header'
 
 import Footer from '../components/Footer/Footer'
+import Banner from '../components/Banner/Banner'
 
 function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
   )
