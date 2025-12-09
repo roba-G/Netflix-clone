2130eb35113cd8a9c329a8c74bfb44a0c579d8bb
roba996 <124337124+roba996@users.noreply.github.com>
2025-12-09 22:26:52 +0300

    kkg

---
+++ netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RowList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RowList
---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
